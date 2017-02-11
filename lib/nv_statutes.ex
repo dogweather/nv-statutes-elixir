aa1cb3ea0e0aa84c10b6bf3b4991cb80e4a879d5
Robb Shecter <robb@weblaws.org>
2017-02-11 01:17:44 -0800

    refactoring

--- lib/nv_statutes.ex
+++ lib/nv_statutes.ex
@@ -18,13 +18,17 @@ defmodule NRS do
 
 
   def title_count(html) do
-    Floki.find(html, ".MsoNormalTable tr")
+    content_rows(html)
     |> Enum.filter(&is_title_row/1)
     |> length
   end
 
 
-  def is_title_row({_, _, tds}) do
-    length(tds) == 1
+  def content_rows(html) do
+    Floki.find(html, ".MsoNormalTable tr")
   end
+
+
+  def is_title_row({_, _, [_td]}), do: true
+  def is_title_row({_, _, _}),  do: false
 end
