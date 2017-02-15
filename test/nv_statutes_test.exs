5f7a45df098131553c450bde516f95233c976566
Robb Shecter <robb@weblaws.org>
2017-02-15 12:51:32 -0800

    cleanup

--- test/nv_statutes_test.exs
+++ test/nv_statutes_test.exs
@@ -1,18 +1,20 @@
 defmodule NvStatutesTest do
   use ExUnit.Case
-  doctest NRS
 
   setup_all do
     {:ok, html: File.read!("nrs.html")}
   end
 
+
   test "finds the correct number of titles", state do
     assert 59 == length(NRS.titles(state[:html]))
   end
 
+
   test "gets the first title's name", state do
     assert List.first(NRS.titles(state[:html])).name == "STATE JUDICIAL DEPARTMENT"
   end
+
 
   test "gets the first title's number", state do
     assert List.first(NRS.titles(state[:html])).number == 1
